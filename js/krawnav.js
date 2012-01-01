4f63119a9b70cc1228e46381ed0f21d872326720
KungD <david@krawaller.se>
2012-01-01 15:58:20 +0100

    navmenu

--- js/krawnav.js
+++ js/krawnav.js
@@ -4,11 +4,33 @@ function makeid(text){
 	return text;
 }
 
+function buildmenusection(branch,depth){
+	var html = "<a class='krawmenulink krawmenulinkdepth"+depth+"' href='#"+branch.id+"'>"+branch.text+"</a><br/>";
+	branch.subs.forEach(function(subtree){
+		html += buildmenusection(subtree,depth+1);
+	});
+	return html;
+}
+
+function setupkrawnav(contentid,navbuttonid,containerid){
+	var tree = krawnav(contentid),
+		menu = "",
+		container = $(containerid).addClass("krawcontentlist");
+	tree.forEach(function(branch){
+		menu += buildmenusection(branch,0);
+	});
+	$(navbuttonid).click(function(e){
+		container.toggleClass("krawcontentlistshow")
+	});
+	container.html(menu);
+}
+
 function krawnav(frag,depth){
 	frag = $(frag || "body");
 	depth = depth || 1;
 	var headings = $("h"+depth,frag),
-		navid = id++;
+		navid = id++,
+		arr = [];
 	headings.each(function(i,el){
 		var me = $(el),
 			next = i < headings.length-1 ? $(headings[i+1]) : me.nextAll("h"+(depth-1)+":first"),
@@ -17,12 +39,18 @@ function krawnav(frag,depth){
 	});
 	var html = "<ul class='krawnav' id='krawsection"+navid+"'>"
 	frag.find(".krawsection").each(function(i,el){
-		var $el = $(el);
-		html += "<li><a href='#"+el.id+"' class='krawlink"+(i?"":" krawlinkactive")+"' id='"+el.id+"link'>"+$(headings[i]).text()+"</a></li>"
-		krawnav(el,depth+1);
+		var $el = $(el),
+			headtext = $.trim($(headings[i]).text());
+		html += "<li><a href='#"+el.id+"' class='krawlink"+(i?"":" krawlinkactive")+"' id='"+el.id+"link'>"+headtext+"</a></li>"
+		arr.push({
+			id: el.id,
+			text: headtext,
+			subs: krawnav(el,depth+1)
+		});
 	});
 	html+="</ul>";
 	frag.find(".krawsection:first").before(html);
+	return arr;
 }
 
 function makesectionchoise(sec){
@@ -51,6 +79,6 @@ function checkHash(){
 setInterval(checkHash,100);
 
 $(function(){
-	krawnav("#main");
+	setupkrawnav("#main","#krawnavbutton","#krawnavcontainer");
 });
 
